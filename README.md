57e4ad346272b625776ec77208e41c03a1e40a23
Marius Grande Andersen <marius.grande.andersen@sikri.no>
2022-12-02 12:12:30 +0100

    made new package + fixed issue with pasting after copying an image from Word

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # YetiForce Ckeditor image to base64 plugin for CKEditor 4
 
-[![npm package](https://img.shields.io/npm/v/ckeditor-image-to-base/latest.svg)](https://www.npmjs.com/package/ckeditor-image-to-base)
-[![Known Vulnerabilities](https://snyk.io//test/github/YetiForceCompany/ckeditor-image-to-base64/badge.svg?targetFile=package.json)](https://snyk.io//test/github/YetiForceCompany/ckeditor-image-to-base64?targetFile=package.json)
-
-Created by YetiForce Sp. z o.o. https://yetiforce.com
+Created by YetiForce Sp. z o.o. https://yetiforce.com and Marius Grande Andersen
 
 ## Requirements
 
@@ -11,15 +8,10 @@ The Browser must support the JavaScript File API.
 
 ## Installation
 
-npm i ckeditor-image-to-base
+"yarn add ckeditor-image-to-base64-mga"
 
-1.  Download the plugin from https://github.com/YetiForceCompany/ckeditor-image-to-base64/archive/master.zip
-
-2.  Extract (decompress) the downloaded file into the plugins folder of your
-    CKEditor installation.
-
-3.  Enable the plugin by using the extraPlugins configuration setting.
-    Example: CKEDITOR.config.extraPlugins = "ckeditor-image-to-base64";
+Enable the plugin by using the extraPlugins configuration setting.
+Example: CKEDITOR.config.extraPlugins = "ckeditor-image-to-base64";
 
 ## License
 
--- plugin.js
+++ plugin.js
@@ -3,15 +3,15 @@
 function initPasteEvent(editorInstance) {
   if (editorInstance.addFeature) {
     editorInstance.addFeature({
-      allowedContent: 'img[alt,id,!src]{width,height};'
+      allowedContent: "img[alt,id,!src]{width,height};",
     });
   }
 
-  editorInstance.on('contentDom', function() {
+  editorInstance.on("contentDom", function () {
     var editableElement = editorInstance.editable
       ? editorInstance.editable()
       : editorInstance.document;
-    editableElement.on('paste', onPaste, null, { editor: editorInstance });
+    editableElement.on("paste", onPaste, null, { editor: editorInstance });
   });
 }
 function onPaste(event) {
@@ -24,11 +24,14 @@ function onPaste(event) {
   if (!clipboardData) {
     return;
   }
-  return Array.prototype.forEach.call(clipboardData.types, function(type, i) {
+  return Array.prototype.forEach.call(clipboardData.types, function (type, i) {
     if (found) {
       return;
     }
-    if (type.match(imageType) || clipboardData.items[i].type.match(imageType)) {
+    if (
+      clipboardData.items[i].kind != "string" &&
+      (type.match(imageType) || clipboardData.items[i].type.match(imageType))
+    ) {
       readImageAsBase64(clipboardData.items[i], editor);
       return (found = true);
     }
@@ -36,80 +39,80 @@ function onPaste(event) {
 }
 
 function readImageAsBase64(item, editor) {
-  if (!item || typeof item.getAsFile !== 'function') {
+  if (!item || typeof item.getAsFile !== "function") {
     return;
   }
   var file = item.getAsFile();
   var reader = new FileReader();
-  reader.onload = function(evt) {
-    var element = editor.document.createElement('img', {
+  reader.onload = function (evt) {
+    var element = editor.document.createElement("img", {
       attributes: {
-        src: evt.target.result
-      }
+        src: evt.target.result,
+      },
     });
-    setTimeout(function() {
+    setTimeout(function () {
       editor.insertElement(element);
     }, 10);
   };
   reader.readAsDataURL(file);
 }
 
-CKEDITOR.plugins.add('ckeditor-image-to-base', {
-  requires: 'dialog',
-  icons: 'ckeditor-image-to-base',
+CKEDITOR.plugins.add("ckeditor-image-to-base", {
+  requires: "dialog",
+  icons: "ckeditor-image-to-base",
   hidpi: true,
-  init: function(editorInstance) {
+  init: function (editorInstance) {
     initPasteEvent(editorInstance);
-    var pluginName = 'ckeditor-image-to-base-dialog';
-    editorInstance.ui.addToolbarGroup('ckeditor-image-to-base', 'insert');
-    editorInstance.ui.addButton('ckeditor-image-to-base', {
+    var pluginName = "ckeditor-image-to-base-dialog";
+    editorInstance.ui.addToolbarGroup("ckeditor-image-to-base", "insert");
+    editorInstance.ui.addButton("ckeditor-image-to-base", {
       label: editorInstance.lang.common.image,
       command: pluginName,
-      toolbar: 'insert'
+      toolbar: "insert",
     });
     CKEDITOR.dialog.add(
       pluginName,
-      this.path + 'dialogs/ckeditor-image-to-base.js'
+      this.path + "dialogs/ckeditor-image-to-base.js"
     );
     editorInstance.addCommand(
       pluginName,
       new CKEDITOR.dialogCommand(pluginName, {
         allowedContent:
-          'img[alt,!src]{border-style,border-width,float,height,margin,margin-bottom,margin-left,margin-right,margin-top,width}',
-        requiredContent: 'img[alt,src]',
+          "img[alt,!src]{border-style,border-width,float,height,margin,margin-bottom,margin-left,margin-right,margin-top,width}",
+        requiredContent: "img[alt,src]",
         contentTransformations: [
-          ['img{width}: sizeToStyle', 'img[width]: sizeToAttribute'],
-          ['img{float}: alignmentToStyle', 'img[align]: alignmentToAttribute']
-        ]
+          ["img{width}: sizeToStyle", "img[width]: sizeToAttribute"],
+          ["img{float}: alignmentToStyle", "img[align]: alignmentToAttribute"],
+        ],
       })
     );
-    editorInstance.on('doubleclick', function(evt) {
+    editorInstance.on("doubleclick", function (evt) {
       if (
         evt.data.element &&
         !evt.data.element.isReadOnly() &&
-        evt.data.element.getName() === 'img'
+        evt.data.element.getName() === "img"
       ) {
         evt.data.dialog = pluginName;
         editorInstance.getSelection().selectElement(evt.data.element);
       }
     });
     if (editorInstance.addMenuItem) {
-      editorInstance.addMenuGroup('imageToBase64Group');
-      editorInstance.addMenuItem('imageToBase64Item', {
+      editorInstance.addMenuGroup("imageToBase64Group");
+      editorInstance.addMenuItem("imageToBase64Item", {
         label: editorInstance.lang.common.image,
-        icon: this.path + 'icons/ckeditor-image-to-base.png',
+        icon: this.path + "icons/ckeditor-image-to-base.png",
         command: pluginName,
-        group: 'imageToBase64Group'
+        group: "imageToBase64Group",
       });
     }
     if (editorInstance.contextMenu) {
-      editorInstance.contextMenu.addListener(function(element, selection) {
-        if (element && element.getName() === 'img') {
+      editorInstance.contextMenu.addListener(function (element, selection) {
+        if (element && element.getName() === "img") {
           editorInstance.getSelection().selectElement(element);
           return { imageToBase64Item: CKEDITOR.TRISTATE_ON };
         }
         return null;
       });
     }
-  }
+  },
 });
